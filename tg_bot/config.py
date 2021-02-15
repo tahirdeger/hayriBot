3937f3ae2bcae8e89b27c8c62daa5581b49b4854
tahirdeger <tahir_deger@hotmail.com>
2021-02-15 17:04:04 +0300

    eklendi

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -5,7 +5,7 @@ class Development(Config):
     OWNER_ID = 824191257
     OWNER_USERNAME = "tahirdeger"
     API_KEY = "1565625697:AAFFDDQKy_1wj7bR9qXRS5_jyEUArrA4nbo"
-    SQLALCHEMY_DATABASE_URI = 'postgres://qoxyayvnwtdvxx:e878f1743845772118f42fe7a87669d5d7d85eb79ff1644ee06b261a5cadb0ef@ec2-52-22-161-59.compute-1.amazonaws.com:5432/dbhnak49mkvs7f'
+    SQLALCHEMY_DATABASE_URI = 'postgres://tuydpfbmqnxefh:afb621068f3a8db5ae7fbe86a905ff5e86481a57660ee1105767c175afdd1986@ec2-34-203-155-237.compute-1.amazonaws.com:5432/dha3kq7sip84r'
     MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
     SUDO_USERS = [824191257]  # List of id's for users which have sudo access to the bot.
