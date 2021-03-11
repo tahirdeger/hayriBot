8fb189a1521b68fa6892093b8a8f9cb18a25e608
tahirdeger <tahir_deger@hotmail.com>
2021-03-11 17:35:45 +0300

    eklendi

--- tg_bot/modules/helper_funcs/string_handling.py
+++ tg_bot/modules/helper_funcs/string_handling.py
@@ -240,7 +240,7 @@ def extract_time(message, time_val):
         unit = time_val[-1]
         time_num = time_val[:-1]  # type: str
         if not time_num.isdigit():
-            message.reply_text("Invalid time amount specified.")
+            message.reply_text("Geçersiz zaman miktarı belirtildi.")
             return ""
 
         if unit == 'm':
@@ -254,5 +254,5 @@ def extract_time(message, time_val):
             return ""
         return bantime
     else:
-        message.reply_text("Invalid time type specified. Expected m,h, or d, got: {}".format(time_val[-1]))
+        message.reply_text("Geçersiz zaman miktarı belirtildi. Önerilen m,h, veya d, {} var".format(time_val[-1]))
         return ""
